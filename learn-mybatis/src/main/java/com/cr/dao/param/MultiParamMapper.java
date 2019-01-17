b0c0033db2b52050d9ecc8731681f4fdbc6938e8
dlinka <dlinka.cg@gmail.com>
2019-01-17 16:16:36 +0800

    init

--- learn-mybatis/src/main/java/com/cr/dao/param/MultiParamMapper.java
+++ learn-mybatis/src/main/java/com/cr/dao/param/MultiParamMapper.java
@@ -3,10 +3,14 @@ package com.cr.dao.param;
 import com.cr.entity.Company;
 import com.cr.entity.User;
 
+import java.util.List;
+
 public interface MultiParamMapper {
 
     User selectUser1(int userId, String name);
 
     User selectUser2(int userId, Company company);
+
+    User selectUser3(List<Integer> userIds, String name);
 
 }
