b2c00f9ef65088f6d98ef80f851843332c33ecae
dlinka <dlinka.cg@gmail.com>
2019-01-29 17:54:51 +0800

    init

--- learn-mybatis/src/main/java/com/cr/entity/City.java
+++ learn-mybatis/src/main/java/com/cr/entity/City.java
@@ -2,8 +2,10 @@ package com.cr.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class City {
+public class City implements Serializable {
 
     private Integer cityId;
     private String cityName;
--- learn-mybatis/src/main/java/com/cr/entity/Company.java
+++ learn-mybatis/src/main/java/com/cr/entity/Company.java
@@ -2,10 +2,11 @@ package com.cr.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class Company {
+public class Company implements Serializable {
 
     private Integer companyId;
     private String companyName;
