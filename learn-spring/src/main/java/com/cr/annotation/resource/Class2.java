57527e2fcbfbed1e7a23941b43e8b6e195e9dcf7
dlinka <dlinka.cg@gmail.com>
2019-01-30 15:11:49 +0800

    init

---
+++ learn-spring/src/main/java/com/cr/annotation/resource/Class2.java
@@ -0,0 +1,8 @@
+package com.cr.annotation.resource;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Class2 {
+
+}
---
+++ learn-spring/src/main/java/com/cr/annotation/resource/Test.java
@@ -0,0 +1,18 @@
+package com.cr.annotation.resource;
+
+import com.cr.annotation.resource.Class1;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("annotation/application.xml");
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+        Class1 class1 = context.getBean("class1", Class1.class);
+        class1.testClass2();
+    }
+
+}
